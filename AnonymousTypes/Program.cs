da3eef2dbf3463076b90d4bbf64d055b3db21c70
TimurAhm <138301367+TimurAhm@users.noreply.github.com>
2024-01-04 21:59:27 +0400

    Add project files.

---
+++ AnonymousTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var o1 = new { Name = "Jeff", Year = 1996 };
+        Console.WriteLine($"{ o1.Name} из {o1.Year}");
+
+
+        var people = new[]
+        {
+            o1,
+            new { Name = "Kristin", Year = 1970 },
+            new { Name = "Timur", Year = 2003 },
+            new { Name = "Grant", Year = 2008},
+        };
+
+        foreach (var person in people)
+        {
+            Console.WriteLine($"{person.Name} {person.Year.ToString()}");
+        }
+    }
+
+    // анонимный тип - олицетворение инкапсуляции
+    // так нызываемая "одноразовая функция"
+}
